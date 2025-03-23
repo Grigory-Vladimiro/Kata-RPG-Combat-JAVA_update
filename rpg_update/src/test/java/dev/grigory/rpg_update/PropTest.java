fd5dc8674c73417f9a9d6150a8a2422e82c13d2a
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-03-24 00:37:07 +0100

    prop_destruction_test

--- rpg_update/src/test/java/dev/grigory/rpg_update/PropTest.java
+++ rpg_update/src/test/java/dev/grigory/rpg_update/PropTest.java
@@ -11,5 +11,11 @@ class PropTest {
         tree.receiveDamage(500);
         assertEquals(1500, tree.getHealth());
     }
-
+    @Test
+    void propIsDestroyedWhenHealthReachesZero() {
+        Prop rock = new Prop(1000);
+        rock.receiveDamage(1000);
+        assertEquals(0, rock.getHealth());
+        assertTrue(rock.isDestroyed());
+    }
 }
